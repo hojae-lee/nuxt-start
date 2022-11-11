534bf7d23240d2d7c014f7a4258505bc9622cd7e
이재호 <jhlee@softin.co.kr>
2022-11-11 17:54:46 +0900

    add: asyncData 내용 추가

---
+++ components/ProductList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+// import axios from 'axios';
+
+export default {
+  // async asyncData() {
+  //   const response = await axios.get('http://localhost:3000/products');
+  //   const products = response.data;
+
+  //   return { products }
+  // }
+}
+</script>
+
+<style>
+
+</style>
--- pages/main.vue
+++ pages/main.vue
@@ -1,12 +1,40 @@
 <template>
   <div>
-    <h1>메인 페이지</h1>
+    <ul>
+      <li v-for="product in products" :key="product.id">
+        <img :src="product.imageUrl" :alt="product.name">
+        <p>{{ product.name }}</p>
+        <p>{{ product.price }}</p>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {
+// eslint-disable-next-line no-unused-vars
+import axios from 'axios';
+// import ProductList from '~/components/ProductList.vue';
 
+export default {
+  components: {
+    // ProductList
+  },
+  // data() {
+  //   return {
+  //     products: []
+  //   }
+  // },
+  // async created() {
+  //   const response = await axios.get('http://localhost:3000/products');
+  //   this.products = response.data;
+  //   console.log(response);
+  // },
+  async asyncData() {
+    const response = await axios.get('http://localhost:3000/products');
+    const products = response.data;
+
+    return { products }
+  }
 }
 </script>
 
