312d9d5da29ac953d23166e0af31b8abc1f0f716
hojae-lee <chosloo2835@gmail.com>
2022-11-16 13:47:35 +0900

    refactoring: fetch 사용 및 시맨틱 구조화

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
     <app-header></app-header>
-    <Nuxt />
+    <app-main></app-main>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
 import AppHeader from '@/components/AppHeader.vue'
+import AppMain from '@/components/AppMain.vue'
+import AppFooter from '~/components/AppFooter.vue'
 
 export default {
   components: {
-    AppHeader
+    AppHeader,
+    AppMain,
+    AppFooter
   }
 }
 </script>
