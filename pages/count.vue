ff61d3efb5e3e7c580c3cb74493c404464ce69a0
hojae-lee <chosloo2835@gmail.com>
2022-11-18 17:50:35 +0900

    css: 간단한 count 페이지 css 스타일링

--- pages/count.vue
+++ pages/count.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>{{ getHeader}} 페이지 입니다.</h1>
     <p>Count: {{ getCount }}</p>
-    <button @click="addCount">눌러보세용.</button>
+    <button class="blueButton" @click="addCount">눌러보세용.</button>
   </div>
 </template>
 
@@ -24,6 +24,43 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+  h1 {
+    color: cornflowerblue
+  }
+  button {
+    box-sizing: border-box;
+    appearance: none;
+    background-color: transparent;
+    border: 2px solid red;
+    border-radius: 0.6em;
+    color: red;
+    cursor: pointer;
+    display: flex;
+    align-self: center;
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1;
+    margin: 20px;
+    padding: 1.2em 2.8em;
+    text-decoration: none;
+    text-align: center;
+    text-transform: uppercase;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 700;
+  }
+  button:hover,
+  button:focus {
+    color: #000;
+    outline: 0;
+  }
+  .blueButton {
+    border-color: blue;
+    color: #fff;
+    box-shadow: 0 0 40px 40px blue inset, 0 0 0 0 blue;
+    transition: all 150ms ease-in-out;
+  }
+  .blueButton:hover {
+    box-shadow: 0 0 10px 0 blue inset, 0 0 10px 4px blue;
+  }
 </style>
