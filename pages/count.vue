7dd0356ced01b1696631cc7fda939778918e1c11
hojae-lee <chosloo2835@gmail.com>
2022-11-18 17:39:44 +0900

    refactoring: this 사용시 eslint error 수정

--- pages/count.vue
+++ pages/count.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
-    <h1>{{ this.$store.state.count.header }} 페이지 입니다.</h1>
-    <p>Count: {{ this.$store.state.count.count }}</p>
+    <h1>{{ getHeader}} 페이지 입니다.</h1>
+    <p>Count: {{ getCount }}</p>
     <button @click="addCount">눌러보세용.</button>
   </div>
 </template>
 
 <script>
 export default {
+  computed: {
+    getHeader() {
+      return this.$store.state.count.header;
+    },
+    getCount() {
+      return this.$store.state.count.count;
+    }
+  },
   methods: {
     addCount() {
       this.$store.commit('count/addCount', 1);
