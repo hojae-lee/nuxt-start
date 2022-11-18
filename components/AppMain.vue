bf4c239d6cc02afd861ce60d5637e12f90310b1f
hojae-lee <chosloo2835@gmail.com>
2022-11-18 18:14:40 +0900

    css: update css

--- components/AppMain.vue
+++ components/AppMain.vue
@@ -10,22 +10,4 @@ export default {
 </script>
 
 <style>
-html,
-body {
-  height: 100%;
-}
-
-body {
-  display: flex;
-  flex-direction: column;
-
-  text-align: center;
-  background-color: #999;
-  margin: 0;
-}
-
-main {
-  flex: 1;
-  background-color: cornflowerblue;
-}
 </style>
